ead10e5eb7605026ac757b0ff106a14da0fb2ad4
Your Name <test@example.com>
2026-03-15 02:24:36 +0500

    feat: add validateInput utility

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,6 @@ export const formatAddress = (address) => {
     if (!address) return '';
     return address.slice(0, 6) + '...' + address.slice(-4);
 };
+
+// validate user input fields
+export const validateInput = (data) => { return data ? JSON.parse(JSON.stringify(data)) : null; };
